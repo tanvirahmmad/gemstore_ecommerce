6af6789c989a051427e5a4fa967013cf4d6d98b2
Khalid <80375483+Khalidhassan3011@users.noreply.github.com>
2024-02-11 22:36:52 +0600

    create a separate route class

---
+++ lib/routing/my_routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:gemstore_ecommerce/features/screens/entry_screens/onboarding_state/on_boarding_state.dart';
+import 'package:gemstore_ecommerce/features/screens/home_screen/home_screen.dart';
+
+class MyRoutes {
+  MyRoutes._();
+
+  static void navigateToHomePage(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()));
+  }
+
+  static void navigateOnBoardingPage(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const OnBoarding()));
+  }
+
+}
