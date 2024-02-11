161ecc8543da879ac4756d3a4067f0d6f9a80fb4
Khalid <80375483+Khalidhassan3011@users.noreply.github.com>
2024-02-11 20:52:34 +0600

    color reformat

--- lib/common/color/my_color.dart
+++ lib/common/color/my_color.dart
@@ -1,7 +1,7 @@
-class MyColor{
-  static const String myColorOne="0xffFFFFFF";
-  static const String myColorTwo="0xff000000";
-  static const String myColorThree= "0xffE7E8E9";
-  static const String myColorFour= " 0xff000000";
-  static const String myColorFive= " 0xff464447";
+class MyColor {
+  static const String myColorOne = "0xffFFFFFF";
+  static const String myColorTwo = "0xff000000";
+  static const String myColorThree = "0xffE7E8E9";
+  static const String myColorFour = "0xff000000";
+  static const String myColorFive = "0xff464447";
 }
