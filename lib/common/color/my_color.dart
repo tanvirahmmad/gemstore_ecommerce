13fa69b601668eb35f353dc633bcd05954e77145
Khalid <80375483+Khalidhassan3011@users.noreply.github.com>
2024-02-11 20:58:40 +0600

    add private constructor

--- lib/common/color/my_color.dart
+++ lib/common/color/my_color.dart
@@ -1,4 +1,6 @@
 class MyColor {
+  MyColor._();
+
   static const String myColorOne = "0xffFFFFFF";
   static const String myColorTwo = "0xff000000";
   static const String myColorThree = "0xffE7E8E9";
