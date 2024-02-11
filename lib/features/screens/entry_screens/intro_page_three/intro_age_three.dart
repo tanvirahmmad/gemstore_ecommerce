224a96704dd664fcce4a67af9ea800379ea8c93a
himel <tanvirahmmadhimel1@gmail.com>
2024-02-11 20:43:52 +0600

    new init

--- lib/features/screens/entry_screens/intro_page_three/intro_age_three.dart
+++ lib/features/screens/entry_screens/intro_page_three/intro_age_three.dart
@@ -22,84 +22,92 @@ class _IntroPageThreeState extends State<IntroPageThree> {
       ),
 
       Container(
-        padding: EdgeInsets.only(top: 780),
         height: 351,
         width: 375,
         color: Colors.white,
       ),
-      Column(
-        children: [
-          Container(
-            child: Center(
-              child: Text(
-                My_Strings.Explore_your_true_style,
-                style: TextStyle(
-                  fontFamily:MY_Assets_Strings.productSans,
-                  color: Color(
-                    int.parse(MyColor.myColorFour)
+      Positioned(top:50 ,
+        left: 55,
+        child: Column(
+          children: [
+            Container(
+              child: Center(
+                child: Text(
+                  My_Strings.Explore_your_true_style,
+                  style: TextStyle(
+                    fontFamily:MY_Assets_Strings.productSans,
+                    color: Color(
+                      int.parse(MyColor.myColorFour)
+                    ),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  fontSize: 20,
+                ),
+              ),
+            ),
+SizedBox(height: 8,),
+            Container(
+              child: Text(
+                My_Strings.Relax_and_let_us_bring_the_style,
+                style: TextStyle(
+                  fontFamily: MY_Assets_Strings.productSans,
+                  color: Color(
+                      int.parse(MyColor.myColorTwo)
+                  ),
+                  fontSize: 14,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-        ],
-      ),
-      Container(
-        padding: EdgeInsets.only(bottom: 450),
-        child: Center(
-          child: Text(
-            My_Strings.Relax_and_let_us_bring_the_style,
-            style: TextStyle(
-              fontFamily: MY_Assets_Strings.productSans,
-              color: Color(
-               int.parse(MyColor.myColorTwo)
-              ),
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-      Center(
-        child: Container(
-          height: 368,
-          width: 261,
-          color: Color(int.parse(MyColor.myColorThree)),
+          ],
         ),
       ),
 
-      Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 0),
+      Positioned( // bottom: MediaQuery.of(context).size.height * 0.2,
+        top: MediaQuery.of(context).size.height * 0.2, // Adjust the top value to center the stack vertically
+        bottom:0 ,// Adjust the bottom value to center the stack vertically
+        left: 50,
+        right:0,
+
+        child: Stack(children: [
+          Container(
+            height: 368,
+            width: 261,
+            color: Color(int.parse(MyColor.myColorThree)),
+          ),
+
+
+        Container(
           child: Image.asset(
             MY_Assets_Strings.intro_page_three_images,
             fit: BoxFit.fitWidth,
           ),
-        ),
+        ),],),
       ),
-      Container(
-        padding: EdgeInsets.only(top: 580, left: 80),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            minimumSize: Size(193, 53),
-            primary: Colors.grey.shade700,
-            side: BorderSide(color: Colors.white, width: 1),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(40)),
+      Positioned(top:MediaQuery.of(context).size.height * 0.880,
+        left:MediaQuery.of(context).padding.left+90,
+
+        child: Container(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(193, 53),
+              primary: Colors.grey.shade700,
+              side: BorderSide(color: Colors.white, width: 1),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(40)),
+              ),
             ),
-          ),
-          onPressed: () {
-            NavigateToNavigationBar();
-          },
-          child: Text(
-            My_Strings.Shopping_now,
-            style: TextStyle(
-              fontFamily: MY_Assets_Strings.productSans,
-              color: Color(int.parse(MyColor.myColorOne)),
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+            onPressed: () {
+              NavigateToNavigationBar();
+            },
+            child: Text(
+              My_Strings.Shopping_now,
+              style: TextStyle(
+                fontFamily: MY_Assets_Strings.productSans,
+                color: Color(int.parse(MyColor.myColorOne)),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
--- lib/features/screens/entry_screens/intro_screens_one/IntroPageOne.dart
+++ lib/features/screens/entry_screens/intro_screens_one/IntroPageOne.dart
@@ -15,89 +15,102 @@ class _IntroPageOneState extends State<IntroPageOne> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(children: [
-      Container(
-        color: Color(int.parse(MyColor.myColorFive)),
-      ),
-      Container(
-        padding: EdgeInsets.only(top: 780),
-        height: 351,
-        width: 375,
-        color: Colors.white,
-      ),
-      Container(
-        padding: EdgeInsets.only(bottom: 520),
-        child: Center(
-          child: Text(
-            My_Strings.Discover_something_new,
-            style: TextStyle(
-              fontFamily: MY_Assets_Strings.productSans,
-              color: Color(
-                int.parse(MyColor.myColorTwo)
+          Container(
+            color: Color(int.parse(MyColor.myColorFive)),
+          ),
+
+          Container(
+            height: 351,
+            width: 375,
+            color: Colors.white,
+          ),
+          Positioned(top:50 ,
+            left: 55,
+            child: Column(
+              children: [
+                Container(
+                  child: Center(
+                    child: Text(
+                      My_Strings.Explore_your_true_style,
+                      style: TextStyle(
+                        fontFamily:MY_Assets_Strings.productSans,
+                        color: Color(
+                            int.parse(MyColor.myColorFour)
+                        ),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8,),
+                Container(
+                  child: Text(
+                    My_Strings.Relax_and_let_us_bring_the_style,
+                    style: TextStyle(
+                      fontFamily: MY_Assets_Strings.productSans,
+                      color: Color(
+                          int.parse(MyColor.myColorTwo)
+                      ),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned( // bottom: MediaQuery.of(context).size.height * 0.2,
+            top: MediaQuery.of(context).size.height * 0.2, // Adjust the top value to center the stack vertically
+            bottom:0 ,// Adjust the bottom value to center the stack vertically
+            left: 50,
+            right:0,
+
+            child: Stack(children: [
+              Container(
+                height: 368,
+                width: 261,
+                color: Color(int.parse(MyColor.myColorThree)),
               ),
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+
+
+              Container(
+                child: Image.asset(
+                  MY_Assets_Strings.intro_page_three_images,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),],),
           ),
-        ),
-      ),
-      Container(
-        padding: EdgeInsets.only(bottom: 450),
-        child: Center(
-          child: Text(
-            My_Strings.Special_new_arrivals,
-            style: TextStyle(
-              fontFamily: MY_Assets_Strings.productSans,
-              color: Color(
-                int.parse(MyColor.myColorTwo)
+          Positioned(top:MediaQuery.of(context).size.height * 0.880,
+            left:MediaQuery.of(context).padding.left+90,
+
+            child: Container(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(193, 53),
+                  primary: Colors.grey.shade700,
+                  side: BorderSide(color: Colors.white, width: 1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                  ),
+                ),
+                onPressed: () {
+                  NavigateToNavigationBar();
+                },
+                child: Text(
+                  My_Strings.Shopping_now,
+                  style: TextStyle(
+                    fontFamily: MY_Assets_Strings.productSans,
+                    color: Color(int.parse(MyColor.myColorOne)),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
-      ),
-      Center(
-        child: Container(
-          height: 368,
-          width: 261,
-          color: Color(int.parse(MyColor.myColorThree)),
-        ),
-      ),
-      Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 19),
-          child: Image.asset(
-            MY_Assets_Strings.intro_page_one_images,
-            fit: BoxFit.fitWidth,
-          ),
-        ),
-      ),
-      Container(
-        padding: EdgeInsets.only(top: 580, left: 80),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            minimumSize: Size(193, 53),
-            primary: Colors.grey.shade700,
-            side: BorderSide(color: Colors.white, width: 1),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(40)),
-            ),
-          ),
-          onPressed: () {
-NavigateToNavigationBar();
-          },
-          child: Text(
-            My_Strings.Shopping_now,
-            style: TextStyle(
-              fontFamily: MY_Assets_Strings.productSans,
-              color: Color(int.parse(MyColor.myColorOne)),
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    ]));
+        ]));
   }
   NavigateToNavigationBar() {
     Navigator.push(
