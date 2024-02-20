ca21cfb08bb21021100b51965dbf0508880c2e72
Khalid <80375483+Khalidhassan3011@users.noreply.github.com>
2024-02-21 03:23:40 +0600

    add color format

--- lib/common/color/my_color.dart
+++ lib/common/color/my_color.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 class MyColor {
   MyColor._();
 
@@ -5,7 +7,7 @@ class MyColor {
   static const String myColorTwo = "0xff000000";
   static const String myColorThree = "0xffE7E8E9";
   static const String myColorFour = "0xff000000";
-  static const String myColorFive = "0xff464447";
+  static const Color myColorFive = Color(0xff464447);
   static const String myColorSix = "0xff1D1F22";
   static const String myColorSeven = "0xff7376BD";
   static const String myColorEight = "0xff1D1F22";
