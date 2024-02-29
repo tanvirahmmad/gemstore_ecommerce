63ce4bf2de5699913e9ec590d5b6bf57007ab21e
Khalid <80375483+Khalidhassan3011@users.noreply.github.com>
2024-03-01 03:35:15 +0600

    hive base structure done

---
+++ lib/data/local_storage/product_storage/product_storage_manager.dart
@@ -0,0 +1,17 @@
+import '../../../models/product_response.dart';
+
+class ProductStorageManager {
+
+  Future<void> addToFavorite(Product product) async {
+
+  }
+
+  static Future<List<Product>> getAllFavoriteProducts() async {
+    return [];
+  }
+
+  static Future<void> removeFromFavorite(Product product) async {
+
+  }
+
+}
