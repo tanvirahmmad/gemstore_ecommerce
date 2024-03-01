e6c7865d6dec8f6da2c4f2dd3b893cf86068b71e
Khalid <80375483+Khalidhassan3011@users.noreply.github.com>
2024-03-02 02:01:59 +0600

    generate order model

---
+++ lib/models/order_model.dart
@@ -0,0 +1,22 @@
+import 'package:gemstore_ecommerce/models/cart_product.dart';
+import 'package:gemstore_ecommerce/models/coupon.dart';
+
+class OrderModel {
+  // List<CartProduct> cartProduct;
+  // String name;
+  // String name;
+  // String name;
+  // String name;
+  // String name;
+  // String name;
+  //
+  // String shippingMethod;
+  //
+  // Coupon coupon;
+  //
+  // String paymentMethod;
+  //
+  // String orderId;
+  // String status;
+  // String orderDate;
+}
