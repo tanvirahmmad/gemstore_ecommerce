23c2ce17bf586ccda69c4b46066cb930981ac8b8
Khalid <80375483+Khalidhassan3011@users.noreply.github.com>
2024-03-02 02:02:17 +0600

    add more feature function name

--- lib/data/local_storage/product_storage/product_storage_manager.dart
+++ lib/data/local_storage/product_storage/product_storage_manager.dart
@@ -1,14 +1,20 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:gemstore_ecommerce/models/cart_product.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import '../../../models/notifications.dart';
+import '../../../models/order_model.dart';
 import '../../../models/product_response.dart';
 
 class ProductStorageManager {
   
   ValueNotifier<List<Product>> favoriteProducts = ValueNotifier([]);
+  ValueNotifier<List<CartProduct>> cartProducts = ValueNotifier([]);
+  ValueNotifier<List<OrderModel>> myOrders = ValueNotifier([]);
+  ValueNotifier<List<Notifications>> notifications = ValueNotifier([]);
 
   ValueNotifier<bool> loading = ValueNotifier(false);
 
@@ -28,6 +34,7 @@ class ProductStorageManager {
     loading.value = false;
   }
 
+  /// favorite
   Future<void> addToFavorite(Product product) async {
     loading.value = true;
 
@@ -71,5 +78,25 @@ class ProductStorageManager {
 
     print("Product in favorite -> ${favoriteProducts.value.length}");
   }
+
+  /// cart
+  Future<void> addToCart(CartProduct cartProduct) async {
+
+  }
+
+  Future<void> deleteFromCart(CartProduct cartProduct) async {
+
+  }
+
+  Future<void> updateQuantity(CartProduct cartProduct) async {
+
+  }
+
+  /// order
+  Future<void> placeOrder(OrderModel cartProduct) async {
+
+  }
+
+  /// notifications
 
 }
